cc6afff7dc72bc520d50fa12c52f6574dd4506a3
hstehli <hugo.stehli1234@gmail.com>
2016-11-02 17:27:22 +0100

    Contact + Page spécifique
    -form
    -tableau
    -debut responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-$( document ).ready(function() {
-  
-       
+$( document).ready(function() {
+    
+    $('#zx-oui').click(function() {
+        if($('#zx-oui').attr("checked", "checked")){
+            $('.choix-modele').toggle();
+        };
+    });
+     $('#zx-non').click(function() {
+        if($('#zx-non').attr("checked", "checked")){
+            $('.choix-modele').hide();
+        };
+    });
 });   
